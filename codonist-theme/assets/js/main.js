1026d84daf8ec9513e93c34101832b98e47e0859
zakaullah khan <zakaullahkhan@codonist.com>
2021-02-17 15:54:40 +0500

    Heading top work complete

--- codonist-theme/assets/js/main.js
+++ codonist-theme/assets/js/main.js
@@ -1,18 +1,5 @@
 
-/*---------------------------DIGIT SECTION---------------------------*/
-// $(document).ready(function(){
-//     $('.list').click(function(){
-//         const val = $(this).attr('data-filter');
-//         if(value == 'all'){
-//             $('.itembox').show('1000');
-//         }
-//         else{
-//             $('.itembox').not('.'+value).hide('1000');
-//             $('.itembox').filter('.'+value).show('1000');
-//         }
-//     })
-// })
-
+/*---------------------------Works Section------------------------------*/
 filterSelection("all")
 function filterSelection(c) {
   var x, i;
